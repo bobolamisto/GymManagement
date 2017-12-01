6215fcfcff7800e4719a1080b638bd497fe8b847
Alina <alina.toaderr@yahoo.com>
2017-12-01 19:32:52 +0200

    implemented the home page

---
+++ GymManagement/GymManagement/Scripts/layout.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var url = $(location).attr('href');
+    if (url.indexOf("Home/Index") >= 0)
+    {
+        $("#body-content").removeClass("container body-content wrapper");
+    }
+    else {
+        $("#body-content").addClass("container body-content wrapper");
+    }
+});
